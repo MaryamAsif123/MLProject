0a99bd7c53cea30b81441b34302ae1eb5d95c02f
MaryamAsif123 <maryambutt260@gmail.com>
2024-09-04 11:05:06 +0500

    model training

--- src/utils.py
+++ src/utils.py
@@ -26,14 +26,9 @@ def evaluate_models(x_train, x_test, y_train, y_test, models):
         for i in range(len(list(models))):
             model= list(models.values())[i]
             model.fit(x_train,y_train)
-            y_train_pred = model.predict(x_train)
-
+            
             y_test_pred = model.predict(x_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
-
             reports[list(models.keys())[i]] = test_model_score
 
         return reports
